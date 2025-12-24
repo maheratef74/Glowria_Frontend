6b4478eb39aa4ff7f78b51b6953fa1b5db35a345
Maher Atef <maheratef600@gmail.com>
2025-12-24 22:36:55 +0200

    Change the domain to the production domain

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5121/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://glowria.runasp.net/api';
 
 export default API_BASE_URL;

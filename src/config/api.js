399fe474ce3f5f5b8d899d6bc6a4c36ffd9167d6
Maher Atef <maheratef600@gmail.com>
2025-12-25 17:59:55 +0200

    change the api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://glowria.runasp.net/api';
+const API_BASE_URL = 'http://glowria.runasp.net/api';
 
 export default API_BASE_URL;
